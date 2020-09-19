93411a82b60183c1588031ef9b6eb68a97214b66
sjoerd-vogel <sjoerd-vogel@users.noreply.github.com>
2020-09-19 18:23:12 +0200

    slightly more convenient coordinate constructor

--- src/main/kotlin/SquareNineBoard.kt
+++ src/main/kotlin/SquareNineBoard.kt
@@ -5,7 +5,7 @@ import SquareNineBoard.State.Fixed
 class SquareNineBoard {
     private val values: Set<Int> = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
     private val cells: List<Cell<out Int>> = (1..9).selfTensor()
-            .map { Coord(it.first, it.second) }
+            .map { Coord(it) }
             .map { it.initialCell() }
 
     override fun toString(): String = cells.map { it.toString() }
@@ -26,6 +26,8 @@ class SquareNineBoard {
     )
 
     private data class Coord(val column: Int, val row: Int) {
+        constructor(pair: Pair<Int, Int>) : this(pair.first, pair.second)
+
         override fun toString(): String = "(column=${column},row=${row})"
     }
 
