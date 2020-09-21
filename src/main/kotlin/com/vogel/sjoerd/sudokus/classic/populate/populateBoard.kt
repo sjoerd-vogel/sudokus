4cbcf597589d32e918bcc116a7c85c2f40873726
sjoerd-vogel <sjoerd-vogel@users.noreply.github.com>
2020-09-22 00:55:34 +0200

    move board creation to more reasonable spot

--- src/main/kotlin/com/vogel/sjoerd/sudokus/classic/populate/populateBoard.kt
+++ src/main/kotlin/com/vogel/sjoerd/sudokus/classic/populate/populateBoard.kt
@@ -7,21 +7,21 @@ import com.vogel.sjoerd.sudokus.classic.createClassicBoard
 //add elements to cells of classic board
 internal tailrec fun <T> populateBoard(elements: Iterable<T>): Board<T> {
     try {
-        return fillSectors(elements)
+        return fillSectors(createClassicBoard(), elements)
     } catch (re: RetryException) {
     }
     return populateBoard(elements)
 }
 
-private fun <T> fillSectors(elements: Iterable<T>): Board<T> {
+private fun <T> fillSectors(board: Board<T>, elements: Iterable<T>): Board<T> {
 
-    tailrec fun worker(board: Board<T> = createClassicBoard()): Board<T> {
+    tailrec fun worker(board: Board<T>): Board<T> {
         val empties = board.getUnfilledSectors()
 
         return if (empties.none()) board
         else worker(populateSector(board, empties.first(), elements))
     }
-    return worker()
+    return worker(board)
 }
 
 private fun <T> Board<T>.getUnfilledSectors() = sectors.filter { sec ->
