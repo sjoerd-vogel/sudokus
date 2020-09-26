3e67df50e6bc73efe370b360e33e6aa6e30b850b
sjoerd-vogel <sjoerd-vogel@users.noreply.github.com>
2020-09-26 15:27:50 +0200

    added parse test helper

--- src/main/kotlin/classic/createClassicBoard.kt
+++ src/main/kotlin/classic/createClassicBoard.kt
@@ -7,7 +7,7 @@ import board.Sector
 import selfTensor
 
 //create a classic sudoku board
-internal fun <T> createClassicBoard(): Board<T> = Board(defaultSectors(), defaultCells())
+fun <T> createClassicBoard(): Board<T> = Board(defaultSectors(), defaultCells())
 
 private fun <T> defaultCells(): Iterable<Cell<T>> = (1..9).selfTensor()
     .map { (column, row) -> Cell(Coord(column, row)) }
--- src/test/kotlin/classic/SectorCompatible.kt
+++ src/test/kotlin/classic/SectorCompatible.kt
@@ -7,6 +7,7 @@ import board.State
 import classic.populate.areSectorsHorizontalCompatible
 import classic.populate.areSectorsVerticalCompatible
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -51,6 +52,40 @@ class SectorCompatible {
             board.sectors.first { Coord(1, 4) in it.coords })
     }(verGood.transpose()))
 
+    @Test
+    fun parse() {
+        val str = "-------------------------------------\n" +
+                "| 5 | 1 | 7 | 3 | 2 | 6 | 9 | 4 | 8 |\n" +
+                "-------------------------------------\n" +
+                "| 4 | 2 | 9 | 5 | 8 | 7 | 3 | 1 | 6 |\n" +
+                "-------------------------------------\n" +
+                "| 8 | 6 | 3 | 1 | 9 | 4 | 2 | 7 | 5 |\n" +
+                "-------------------------------------\n" +
+                "| 7 | 4 | 5 | 8 | 3 | 2 | 1 | 6 | 9 |\n" +
+                "-------------------------------------\n" +
+                "| 1 | 8 | 6 | 4 | 5 | 9 | 7 | 3 | 2 |\n" +
+                "-------------------------------------\n" +
+                "| 9 | 3 | 2 | 6 | 7 | 1 | 8 | 5 | 4 |\n" +
+                "-------------------------------------\n" +
+                "| 2 | 9 | 4 | 7 | 1 | 5 | 6 | 8 | 3 |\n" +
+                "-------------------------------------\n" +
+                "| 6 | 7 | 8 | 9 | 4 | 3 | 5 | 2 | 1 |\n" +
+                "-------------------------------------\n" +
+                "| 3 | 5 | 1 | 2 | 6 | 8 | 4 | 9 | 7 |\n" +
+                "-------------------------------------"
+        assertEquals(str.parse().toString(), str)
+        assertEquals(
+            str
+                .replace("-", "")
+                .replace("|", "")
+                .replace(" ", "")
+                .replace("\n", "")
+                .parse().toString(),
+            str
+        )
+
+    }
+
     private fun <T> Board<T>.transpose(): Board<T> {
         val empty = Board<T>(sectors, cells.map { Cell(it.coord, State.Empty) })
         tailrec fun work(cells: Iterable<Cell<T>>, board: Board<T>): Board<T> {
@@ -65,4 +100,26 @@ class SectorCompatible {
 
     private fun Coord.transpose(): Coord = Coord(row = column, column = row)
     private fun <T> Iterable<T>.tail() = drop(1)
+
+    private fun String.parse(): Board<String> {
+        val instruction = replace("-", "")
+            .replace("|", "")
+            .replace(" ", "")
+            .replace("\n", "")
+
+        fun Board<String>.firstEmpty(): Coord? = cells.firstOrNull() { it.state == State.Empty }?.coord
+        tailrec fun work(
+            board: Board<String> = createClassicBoard(),
+            toDo: String = instruction
+        ): Board<String> {
+            if (board.firstEmpty() == null) return board
+            if (toDo.isEmpty()) return board
+
+            return work(board.set(board.firstEmpty()!!, toDo.first().toString()), toDo.substring(1))
+
+        }
+
+
+        return work().transpose()
+    }
 }
