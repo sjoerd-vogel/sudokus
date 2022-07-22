dc7c68a6cf7ca59fb9dfaa89cf8ae8dd413d7f74
sjoerd-vogel <sjoerd-vogel@users.noreply.github.com>
2022-07-22 14:10:35 +0200

    setup project and Iterable utility methods

---
+++ README.md
@@ -0,0 +1,6 @@
+# About sudokus
+This is an attempt at writing a sudoku generator. It is currently a work in progress. For now this project uses gradle without gradle wrapper (I refuse to commit binaries because of a design decision made by a build-tool). This project is currently very volatile and may break at any moment, please dont use as a dependency.
+
+build project using `gradle build` with a sufficiently recent version of gradle and java version 18
+
+I will try to not force push to master. No promises. Prefer git fetch/reset workflows over pulls.
---
+++ src/test/kotlin/sudokus/IterableExtensionsKtTest.kt
@@ -0,0 +1,55 @@
+package sudokus
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+internal class IterableExtensionsKtTest {
+
+    @Test
+    fun itShouldProperlyTensorTogetherBothLists() {
+        assertEquals(
+            listOf(
+                Pair(1, 4), Pair(1, 5), Pair(1, 6),
+                Pair(2, 4), Pair(2, 5), Pair(2, 6),
+                Pair(3, 4), Pair(3, 5), Pair(3, 6),
+            ),
+
+            (1..3).tensor(4..6)
+        )
+    }
+
+    @Test
+    fun itShouldProperlyTensorTheListWithItself() {
+        assertEquals(
+            listOf(
+                Pair(1, 1), Pair(1, 2), Pair(1, 3),
+                Pair(2, 1), Pair(2, 2), Pair(2, 3),
+                Pair(3, 1), Pair(3, 2), Pair(3, 3),
+            ),
+
+            (1..3).selfTensor()
+        )
+    }
+
+    @Test
+    fun itShouldConcludeAllValuesAreTrue() {
+        assertTrue(listOf(true, true, true).allTrue())
+    }
+
+    @Test
+    fun itShouldConcludeNotAllValuesAreTrue() {
+        assertFalse(listOf(true, false, true).allTrue())
+    }
+
+    @Test
+    fun itShouldConcludeAllValuesAreFalse() {
+        assertTrue(listOf(false, false, false).allFalse())
+    }
+
+    @Test
+    fun itShouldConcludeNotAllValuesAreFalse() {
+        assertFalse(listOf(true, false, true).allFalse())
+    }
+}
